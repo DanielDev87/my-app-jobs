4a34f27b3c209903815d3abb507328ea0c00ff02
DanielDev87 <danielhymtj@icloud.com>
2025-03-14 14:53:08 -0500

    Implementando gradientes e iconos

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {StyleSheet, Text} from 'react-native'
+import colors from '../constants/colors'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const UserScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>User Screen 🤓</Text>
-    </View>
+    <LinearGradient colors={colors.gradienteAccion} style={styles.container}>
+          <Text style= {styles.text}>User Screen 🤓</Text>
+    </LinearGradient>
+    
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    color: colors.luminous,
+    fontWeight: 'bold'
+  }
+})
 
 export default UserScreen
